88ab375f4fe2e6d413cba0bbaa2ce1f0cb1fbbb9
dipenvir <dkaur104@my.bcit.ca>
2023-11-08 13:28:29 -0800

    Updating newAccount.js

--- scripts/newAccount.js
+++ scripts/newAccount.js
@@ -2,6 +2,7 @@ function saveUsernamePassword(){
     console.log("inside saving username and password");
     let username = document.getElementById("user").value;
     let password = document.getElementById("passWord").value;
+    let web = document.getElementById("websiteName").value;
     
     console.log(username,password);
     
@@ -9,6 +10,7 @@ function saveUsernamePassword(){
     db.collection("accounts").add({
         user:username,
         passWord:password,
+        websiteName:web,
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
     }).then(() => {
         window.location.href = "passwordmanager.html"; // Redirect to the password manager page
