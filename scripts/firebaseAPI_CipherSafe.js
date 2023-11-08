25cfdfc70fb015663cece496e00a73ad31d7c0e1
dipenvir <dkaur104@my.bcit.ca>
2023-11-08 09:24:48 -0800

    Added firebase js file

---
+++ scripts/firebaseAPI_CipherSafe.js
@@ -0,0 +1,20 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+var firebaseConfig = {
+    
+    apiKey: "AIzaSyD_AHe4v1qbukDQKMK85i37uNC5H6LF1t8",
+    authDomain: "ciphersafe-f1e5c.firebaseapp.com",
+    projectId: "ciphersafe-f1e5c",
+    storageBucket: "ciphersafe-f1e5c.appspot.com",
+    messagingSenderId: "424688529138",
+    appId: "1:424688529138:web:b583db1398c32c7bc5aa6b"
+
+};
+
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
