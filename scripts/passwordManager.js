17a66edb808299c72214eafe59eeb0e91064c5ab
dipenvir <dkaur104@my.bcit.ca>
2023-11-16 09:12:27 -0800

    Updating password manager html and js

--- scripts/passwordManager.js
+++ scripts/passwordManager.js
@@ -10,7 +10,23 @@ function closeOK() {
 }
 const element = document.getElementById("saveBtn");
 
-element.addEventListener("click", function () {
-    document.getElementById("div").style.display = "none";
-    document.getElementById("saved").style.display = "";
-});
+function saveUsernamePassword(){
+    console.log("inside saving username and password");
+    let username = document.getElementById("user").value;
+    let password = document.getElementById("passWord").value;
+    let web = document.getElementById("websiteName").value;
+    
+    console.log(username,password);
+    
+    // Get the document for the current user.
+    db.collection("accounts").add({
+        user:username,
+        passWord:password,
+        websiteName:web,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    }).then(() => {
+        document.getElementById("div").style.display = "none";
+        document.getElementById("saved").style.display = "";
+    });
+
+}
