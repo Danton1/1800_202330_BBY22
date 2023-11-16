3347a34eda7cca7ad8e2e6b9aa031abbdd0c784d
sxht <steven.xht@gmail.com>
2023-11-15 16:46:16 -0800

    headers and footers workwith user login status

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -2,19 +2,15 @@
 // This function loads the parts of your skeleton 
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
-function loadSkeleton() {
-
-  // firebase.auth().onAuthStateChanged(function (user) {
-  //     if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
-  //         // User is signed in.
-  //         // Do something for the user here.
-          // console.log($('#navbarPlaceholder').load('./text/navbar_after_login.html'));
-          console.log($('#footerPlaceholder').load('../text/footer_after_login.html'));
-  //     } else {
-  //         // No user is signed in.
-          console.log($('#navbarPlaceholder').load('./text/navbar_before_login.html'));
-  //         console.log($('#footerPlaceholder').load('../text/footer_before_login.html'));
-  //     }
-  // });
-}
-loadSkeleton(); //invoke the function
+auth.onAuthStateChanged(user => {
+        if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
+                //         // User is signed in.
+                //         // Do something for the user here.
+                console.log($('#navbarPlaceholder').load('./text/navbar_after_login.html'));
+                console.log($('#footerPlaceholder').load('../text/footer_after_login.html'));
+        } else {
+                //         // No user is signed in.
+                console.log($('#navbarPlaceholder').load('./text/navbar_before_login.html'));
+                console.log($('#footerPlaceholder').load('../text/footer_before_login.html'));
+        }
+})
