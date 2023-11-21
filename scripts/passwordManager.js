e1fa721424d210c5cd0c8ce01db08632e3c9ac0c
dipenvir <dkaur104@my.bcit.ca>
2023-11-21 12:59:30 -0800

    Updating the password manager - pm now display saved passwords

--- scripts/passwordManager.js
+++ scripts/passwordManager.js
@@ -1,38 +1,87 @@
 function showDiv() {
-    document.getElementById("div").style.display = "";
+  document.getElementById("div").style.display = "";
 }
 function closeDiv() {
-    document.getElementById("div").style.display = "none";
+  document.getElementById("div").style.display = "none";
 }
 
 function closeOK() {
-    document.getElementById("saved").style.display = "none";
+  document.getElementById("saved").style.display = "none";
 }
 const element = document.getElementById("saveBtn");
 
-function showInfo(){
-    document.getElementById("infoOfUser").style.display= "block";
-    document.getElementById("show").innerHTML = "Hide";
-    document.getElementById("showico").style.display = "contents";
+function showInfo() {
+  document.getElementById("infoOfUser").style.display = "block";
+
+  document.getElementById("showico").style.display = "contents";
 }
 
-function saveUsernamePassword(){
-    console.log("inside saving username and password");
-    let username = document.getElementById("user").value;
-    let password = document.getElementById("passWord").value;
-    let web = document.getElementById("websiteName").value;
-    
-    console.log(username,password);
-    
-    // Get the document for the current user.
-    db.collection("accounts").add({
-        user:username,
-        passWord:password,
-        websiteName:web,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    }).then(() => {
-        document.getElementById("div").style.display = "none";
-        document.getElementById("saved").style.display = "";
+
+
+
+
+
+
+function saveUsernamePassword() {
+  console.log("inside saving username and password");
+  let username = document.getElementById("user").value;
+  let password = document.getElementById("passWord").value;
+  let web = document.getElementById("websiteName").value;
+
+  // console.log(username,password);
+
+  // Get the document for the current user.
+  db.collection("accounts").add({
+    user: username,
+    passWord: password,
+    websiteName: web,
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  }).then(() => {
+    document.getElementById("div").style.display = "none";
+    document.getElementById("saved").style.display = "";
+
+    document.addEventListener('DOMContentLoaded', function () {
+      var toggleButton = document.getElementById('show');
+      var content = document.getElementById('infoOfUser');
+
+      // Add click event listener to the button
+      toggleButton.addEventListener('click', function () {
+        // Toggle the visibility of the content
+        if (content.style.display === 'none') {
+          content.style.display = 'block';
+          toggleButton.innerText = 'Hide';
+        } else {
+          content.style.display = 'none';
+          toggleButton.innerText = 'Show';
+        }
+      });
     });
-
+  })
 }
+
+
+function populatePasswordManager() {
+  console.log("test");
+  let managerTemplate = document.getElementById("managerTemp");
+  db.collection("accounts")
+    .get()
+    .then((allAccounts) => {
+      allAccounts.forEach(doc => {
+        var username = doc.data().user;
+        var password = doc.data().passWord;
+        var web = doc.data().websiteName;
+        console.log(username);
+        console.log(password)
+        console.log(web);
+
+        let managerCard = managerTemplate.content.cloneNode(true);
+
+        managerCard.querySelector("#username").innerHTML = username;
+        managerCard.querySelector("#pass").innerHTML = password;
+        managerCard.querySelector("#website").innerHTML = password;
+        document.getElementById("container").append(managerCard);
+      });
+    });
+}
+
+populatePasswordManager();
