499a7231f76e099863b78e68a7c8882487fac8da
Danton Soares <dantonvsoares@gmail.com>
2023-11-21 01:36:47 -0800

    Adding functionality to password checker, improving its styling and minor overall improvements

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,27 +1,27 @@
-
-
 let username;
 //---------------------------------------------------
 // This function loads template parts 
 // (navbar, footer) into html doc. 
 //---------------------------------------------------
 auth.onAuthStateChanged(user => {
-      if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
-          // If user is signed in, load User navbar and footer
-          console.log($('#navbarPlaceholder').load('./text/navbar_after_login.html'));
-          console.log($('#footerPlaceholder').load('../text/footer_after_login.html'));
-          console.log("user logged in: ", user);
-          username = user.displayName;
-          const usernamePlaceholder = document.querySelector(".username_here");
-          usernamePlaceholder.innerHTML = username;
-      } else {
-          // No user is signed in. Load default navbar and footer
-          console.log($('#navbarPlaceholder').load('./text/navbar_before_login.html'));
-          console.log($('#footerPlaceholder').load('../text/footer_before_login.html'));
-          console.log('No user is signed in');
-          username = "User";
-      }
-  });
+  if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
+    // If user is signed in, load User navbar and footer
+    console.log($('#navbarPlaceholder').load('./text/navbar_after_login.html'));
+    console.log($('#footerPlaceholder').load('../text/footer_after_login.html'));
+    console.log("user logged in: ", user.displayName);
+    username = user.displayName;
+    const usernamePlaceholder = document.querySelectorAll(".usernameHere");
+    usernamePlaceholder.forEach(e => {
+      e.innerHTML = username;
+    })
+  } else {
+    // No user is signed in. Load default navbar and footer
+    console.log($('#navbarPlaceholder').load('./text/navbar_before_login.html'));
+    console.log($('#footerPlaceholder').load('../text/footer_before_login.html'));
+    console.log('No user is signed in');
+    username = "User";
+  }
+});
 
 const section = document.querySelector(".confirmation-modal"),
   overlay = document.querySelector(".confirmation-overlay"),
@@ -30,7 +30,7 @@ const section = document.querySelector(".confirmation-modal"),
   logoutConfirm = document.querySelector("#logoutConfirm");
 logoutBtn.addEventListener("click", () => {
   section.classList.add("active");
-  document.getElementById("confirmMessage").innerHTML = `${username ? username + ", are": "Are"} you sure you want to log out?`;
+  document.getElementById("confirmMessage").innerHTML = `${username ? username + ", are" : "Are"} you sure you want to log out?`;
 });
 overlay.addEventListener("click", () =>
   section.classList.remove("active")
@@ -45,6 +45,6 @@ logoutConfirm.addEventListener('click', (e) => {
     document.getElementById("doorConfirm").className = "fa-solid fa-check-circle";
     document.getElementById("confirmMessage").innerHTML = `${username ? username : "User"} has been logged out.`
   }).then(setTimeout(() => {
-    window.location.href="index.html";
+    window.location.href = "index.html";
   }, 3000));
 })
--- scripts/encryption.js
+++ scripts/encryption.js
@@ -1,3 +1,77 @@
-var encryptedAES = CryptoJS.AES.encrypt("Message", "My Secret Passphrase");
-var decryptedBytes = CryptoJS.AES.decrypt(encryptedAES, "My Secret Passphrase");
-var plaintext = decryptedBytes.toString(CryptoJS.enc.Utf8);
+// var encryptedAES = CryptoJS.AES.encrypt("Message", "My Secret Passphrase");
+// var decryptedBytes = CryptoJS.AES.decrypt(encryptedAES, "My Secret Passphrase");
+// var plaintext = decryptedBytes.toString(CryptoJS.enc.Utf8);
+
+
+// Function to hash a password using SHA-1
+async function hashPassword(password) {
+  // Converting the password string to a buffer
+  const buffer = new TextEncoder().encode(password);
+  // Using the crypto.subtle.digest function to compute the SHA-1 hash of the buffer
+  const hashBuffer = await crypto.subtle.digest('SHA-1', buffer);
+
+  // Converting the hash buffer to a hexadecimal string
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(byte => byte.toString(16).padStart(2, '0')).join('');
+
+  // Returning the hashed password in SHA-1 format
+  return hashHex.toUpperCase();
+}
+
+// Function to check if a password has been pwned (compromised in a data breach)
+async function checkPassword(password) {
+  // Defining a variable to hold the amount of times a password has been pwned
+  let pwnedCount = 0;
+  // Hashing the input password
+  const hashedPassword = await hashPassword(password);
+  // Taking the first 5 characters of the hashed password (k-anonymity model)
+  const FiveCharHashedPassword = hashedPassword.substring(0, 5);
+  // Taking the remainder of the hashed password to becompared to the Pwned Passwords API response
+  const hashedPasswordRemainder = hashedPassword.substring(5);
+
+  // Construct the API URL with the first 5 characters of the hashed password
+  const apiUrl = `https://api.pwnedpasswords.com/range/${FiveCharHashedPassword}`;
+
+  // Send a request to the Pwned Passwords API
+  const response = await fetch(apiUrl);
+  // Check if the request was successful (status code 200)
+  if (response.ok) {
+    return response.text()
+      .then(list => {
+        // Parsing the response body as text, splitting it at every line break
+        let pwd_list = list.split("\r\n");
+        // Iterating through the encrypted passwords list to find the inputted password
+        pwd_list.forEach(e => {
+          let eachPassword = e.substring(0, e.indexOf(":"));
+          if (eachPassword == hashedPasswordRemainder) {
+            // If found, we obtain how many times it's been pwned
+            pwnedCount = parseInt(e.substring(e.indexOf(':') + 1));
+          }
+        });
+        return pwnedCount;
+      }).then(pwnedCount => {
+        if (pwnedCount > 0) {
+          console.log(`This password has been pwned ${pwnedCount} times.`);
+        } else {
+          console.log("This password has not been pwned.");
+        }
+        return pwnedCount;
+      })
+  } else {
+    console.error(`Error checking password: ${response.status} - ${response.statusText}`);
+  }
+}
+
+// const passwordToCheck = "Damage123";
+// let checked = checkPassword(passwordToCheck);
+// console.log(checked)
+// checked.then(a => console.log(a))
+
+async function main() {
+  const passwordToCheck = "Damage123";
+  let checked = await checkPassword(passwordToCheck);
+  console.log(checked);
+}
+
+main();
+
